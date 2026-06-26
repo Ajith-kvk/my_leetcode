10329a32bab05449e6c95f6a055d70aa76112a43
Ajith Kumar Vishwakarma <akvk55555@gmail.com>
2026-06-26 17:25:12 +0530

    Time: 4 ms (96.99%) | Memory: 44.8 MB (75.94%) - LeetSync

---
+++ 71-simplify-path/simplify-path.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Deque<String> stack = new ArrayDeque<>();
+        String[] parts = path.split("/");
+        for (String part : parts) {
+            if (part.equals("") || part.equals(".")) {
+                continue;
+            } else if (part.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.removeLast();
+                }
+            } else {
+                stack.addLast(part);
+            }
+        }
+        if (stack.isEmpty()) {
+            return "/";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String dir : stack) {
+            sb.append("/").append(dir);
+        }
+        return sb.toString();
+    }
+}
